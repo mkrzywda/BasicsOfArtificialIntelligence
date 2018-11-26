d881d8fab32794fbd2d1b6865a0d16892cf36d62
mkrzywda <43749180+mkrzywda@users.noreply.github.com>
2018-11-26 23:37:34 +0100

    Code review.

--- Sprawozdanie_3/feedforward_backpropagation.m
+++ Sprawozdanie_3/feedforward_backpropagation.m
@@ -1,24 +1,6 @@
 close all; clear all; clc;
 
-function fx = rstrigin_3d(x)
-    if x == 0
-        fx = 0;
-    else
-		x1 = x;
-        A = 10;
-        n = 100;
-        dx = (5.12-x)/n;
-        
-		fx = A * n;
-
-        for i = 1:n
-            x = x1 + (i * dx);
-            fx = fx + (x^2) - (A * cos(2 * pi * x));
-        end
-    end
-end
-
-%nasz dataset
+%dataset
 in = [-2.0 -1.8 -1.6 -1.4 -1.2 -1.0 -0.8 -0.6 -0.4 -0.2 0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0]
 out = [1663.3144032672324 1690.1132374370839 1688.034998342087 1674.099059643066 1686.7210309127113 1732.9225480289258 1776.4412985191298 1786.0418485521361 1779.9521156718527 1803.5731229031892 0 1928.860217750531 1949.5489457731007 1948.9857411333403 1982.511002459129 2065.507872849039 2147.692111352819 2178.4193810575716 2179.078110374434 2220.2957248379535 2326.150912968639 ]
 test = zeros(1);
@@ -40,6 +22,25 @@ efect = zeros(size(net));
 
 % genereujemy wyniki dla funkcji rstrigin_3d oraz testujemy dzialanie sieci.
 for k = 1:21
-    test(k) = RastriginTest3D(in(k));
+    test(k) = rstrigin_3d(in(k));
 	efect(k) = sim(net, in(k)); 
 end    
+
+
+function fx = rstrigin_3d(x)
+    if x == 0
+        fx = 0;
+    else
+		x1 = x;
+        A = 10;
+        n = 100;
+        dx = (5.12-x)/n;
+        
+		fx = A * n;
+
+        for i = 1:n
+            x = x1 + (i * dx);
+            fx = fx + (x^2) - (A * cos(2 * pi * x));
+        end
+    end
+end
